b906988d795f6b9532dd0d5a459393b9e055d241
Averydx <79957750+Averydx@users.noreply.github.com>
2022-02-16 08:12:21 -0800

    Add files via upload

---
+++ binaryTreeNode.h
@@ -0,0 +1,11 @@
+#pragma once
+template <class elemType>
+struct binaryTreeNode
+{
+	elemType info; 
+	binaryTreeNode<elemType>*llink; //left node pointer
+	binaryTreeNode<elemType>* rlink; //right node pointer 
+
+
+};
+
